84ed46a3f18a0829699231ee876ed88242bd8594
NicoleAndreaBolus <nagbolus.student@ua.edu.ph>
2025-10-15 09:50:10 +0800

    Added userlist

--- registration/urls.py
+++ registration/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/register/', views.register_user, name='register_user')
+    path('api/register/', views.register_user, name='register_user'),
+    path('api/users/', views.list_users, name='list_users'),
 ]
